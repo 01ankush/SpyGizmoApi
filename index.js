0d8bcb16ca417759ebe953f3e156128035055c7f
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-02-09 09:54:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000;
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:8000', 
+    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
