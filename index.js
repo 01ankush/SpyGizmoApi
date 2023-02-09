8129905c665509efb2a937da5d364213115f0473
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-02-09 11:46:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const users = require('./spy.json');
+const productusers = require('./proa.json');
 const app = express();
 const PORT = 8000;
 const cors = require('cors');
@@ -14,4 +15,13 @@ app.use(cors(corsOptions));
 app.get('/api/products',(req,res)=>{
     return res.json(users);
 })
+
+app.get('/api/products/:id',(req,res)=>{
+    const id = req.params.id;
+    const user = productusers.find((user) => user.id === id);
+    return res.json(user);
+})
+
+
+
 app.listen(PORT,() =>console.log(`Server Started at PORT:${PORT}`));
