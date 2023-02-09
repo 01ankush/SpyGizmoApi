04fff0484059a4ed89787ca5f7dffb06b72178eb
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-02-09 09:51:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const users = require('./MOCK_DATA.json');
+const app = express();
+const PORT = 8000;
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:8000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+// app.use(express.json());
+
+app.get('/api/products',(req,res)=>{
+    return res.json(users);
+})
+app.listen(PORT,() =>console.log(`Server Started at PORT:${PORT}`));
